f123c0e3d76e7d5c9f6ad640cc3cb97de4eea991
Apoorv Dham <apoorv.dham@accenture.com>
2020-07-07 11:40:04 +0530

    task-1.1

---
+++ 1.1/1.1.js
@@ -0,0 +1,48 @@
+//Declare four variables without assigning values and print them in console
+
+let a,b,c,d;
+console.log("a= "+a+" b= "+b+" c= "+c+" d= "+d);
+
+//How to get value of the variable myvar as output
+
+let myvar= 5;
+console.log(myvar);
+
+// Declare variables to store your first name, 
+//last name, marital status, country and age in multiple lines.
+
+let fname = 'apoorv';
+let lname = 'dham';
+let ms = 'single';
+let country='IN';
+let age = 24;
+
+//Declare variables to store your first name, 
+//last name, marital status, country and age in a single line
+
+let fname1= 'apoorv' ,lname1 = 'dham',ms1 = 'single',country1='IN',age1 = 24;
+
+
+//Declare variables and assign string, boolean, undefined and null data types
+
+let a1 ='c';
+let b1 = true;
+let x;
+let y=null;
+
+//Convert the string to integer
+
+console.log(parseInt('25'))
+
+// Write 6 statement which provide truthy & falsey values.
+
+    // 1.  if(x!=='undefined')
+    // 2. while (x!==0) 
+    // 3. (x===3)
+    // 4. if(x>=2)
+    // 5. Boolean(x=0);
+    // 6. if(x== typeof(null))
+         
+
+
+
---
+++ 1.1/README.md
@@ -0,0 +1,1 @@
+ Simple Programs todo for variables
