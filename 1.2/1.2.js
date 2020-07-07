c073def7f68dadd79e6d9f676748dcb4c97f7684
Apoorv Dham <apoorv.dham@accenture.com>
2020-07-07 12:10:52 +0530

    1.2

---
+++ 1.2/1.2.js
@@ -0,0 +1,38 @@
+//Square of a number
+let n1=2;
+console.log(Math.pow(n1,2));
+
+//Swapping 2 numbers
+let temp1;
+let x=12;
+let y=4;
+temp1=y;
+y=x;
+x=temp1;
+console.log("x="+x+"  "+"y="+y);
+
+//Addition of 3 numbers
+let a=1,b=2,c=3;
+console.log(a+b+c);
+
+//Celsius to Fahrenheit conversion
+let celcius = 23;
+console.log("Farheinheit:  "+((9/5)*celcius+32));
+
+//Meter to miles
+let metres = 2.1;
+console.log("miles=  "+metres*0.000621);
+
+//Pounds to kg
+let Pounds=2;
+console.log("kg=   "+Pounds/2.205);
+
+//Calculate Batting Average
+let b1=25,b2=59;
+console.log("average=   "+(b1+b2)/2);
+
+//Calculate five test scores and print their average
+let t1=2,t2=210,t3=78,t4=79,t5=80;
+console.log("test average=   "+(t1+t2+t3+t4+t5)/5);
+
+//Power of any number x ^ y.
