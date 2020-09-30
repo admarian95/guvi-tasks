d9d72ecd1102e3f7004beeaf127f175cb938ef86
Apoorv Dham <apoorv.dham@accenture.com>
2020-10-01 00:18:36 +0530

    loadash

--- Bootcamp/loadash/index.js
+++ Bootcamp/loadash/index.js
@@ -1,3 +1,6 @@
+console.log('------------------------------------------------------');
+console.log('------------------------------------------------------');
+console.log("chunk:");
 var chunk = function (arr, no) {
     var arr1 = [];
     var arr2 = [];
@@ -10,6 +13,9 @@ var chunk = function (arr, no) {
     return [arr1, arr2];
 };
 console.log(chunk([1, 2, 3, 4], 3));
+console.log('------------------------------------------------------');
+console.log('------------------------------------------------------');
+console.log("sum:");
 var sum = function (arr) {
     var s = 0;
     for (var i = 0; i < arr.length; i++) {
@@ -18,11 +24,59 @@ var sum = function (arr) {
     return s;
 };
 console.log(sum([1, 2, 3, 4]));
+console.log('------------------------------------------------------');
+console.log('------------------------------------------------------');
+console.log("filter:");
 var users = [
     { 'user': 'barney', 'age': 36, 'active': true },
-    { 'user': 'fred', 'age': 40, 'active': false }
+    { 'user': 'fred', 'age': 20, 'active': false },
+    { 'user': 'fred', 'age': 45, 'active': true },
+    { 'user': 'fred', 'age': 19, 'active': false },
+    { 'user': 'fred', 'age': 19, 'active': true }
 ];
 var filter = function (arr, f) {
-    console.log(f);
+    for (var i in arr) {
+        if (f(arr[i])) {
+            console.log(arr[i]);
+        }
+    }
 };
-filter(users, function () { 1 + 1; });
+filter(users, function (e) { return e.age > 20; });
+console.log('------------------------------------------------------');
+console.log('------------------------------------------------------');
+console.log("find:");
+var find = function (arr, f) {
+    var foundVal = [];
+    var j = 0;
+    for (var i in arr) {
+        if (f(arr[i])) {
+            foundVal[j] = arr[i];
+            j++;
+        }
+    }
+    if (foundVal.length === 0) {
+        console.log("not found");
+    }
+    else {
+        console.log("filtered obj     =>   ");
+        for (var i in foundVal) {
+            console.log(foundVal[i]);
+        }
+    }
+};
+find(users, function (e) { return e.age === 19 || e.active === true; });
+console.log('------------------------------------------------------');
+console.log('------------------------------------------------------');
+console.log("reduce:");
+var reduce = function (arr, f, initVal) {
+    var initSum = initVal;
+    var array = arr;
+    for (var i = 0; i < array.length; i++) {
+        initSum = f(initSum, arr[i]);
+    }
+    return initSum;
+};
+var ans = reduce([1, 2], function (sum, n) {
+    return sum + n;
+}, 0);
+console.log(ans);
--- Bootcamp/loadash/index.ts
+++ Bootcamp/loadash/index.ts
@@ -1,3 +1,6 @@
+console.log('------------------------------------------------------')
+console.log('------------------------------------------------------')
+console.log("chunk:")
 var chunk =(arr:Array<number>,no:number)=>{
     var arr1=[];
     var arr2=[];
@@ -11,7 +14,9 @@ var chunk =(arr:Array<number>,no:number)=>{
 }
 
 console.log(chunk([1,2,3,4],3));
-
+console.log('------------------------------------------------------')
+console.log('------------------------------------------------------')
+console.log("sum:")
 var sum =(arr:Array<number>)=>{
     let s=0;
     for(let i=0;i<arr.length;i++){
@@ -21,14 +26,65 @@ var sum =(arr:Array<number>)=>{
 }
 
 console.log(sum([1,2,3,4]));
-
+console.log('------------------------------------------------------')
+console.log('------------------------------------------------------')
+console.log("filter:")
 var users = [
     { 'user': 'barney', 'age': 36, 'active': true },
-    { 'user': 'fred',   'age': 40, 'active': false }
+    { 'user': 'fred',   'age': 20, 'active': false },
+    { 'user': 'fred',   'age': 45, 'active': true },
+    { 'user': 'fred',   'age': 19, 'active': false },
+    { 'user': 'fred',   'age': 19, 'active': true }
   ];
-var filter=(arr:Array<object>,  f:CallableFunction )=>{
+var filter=(arr:Array<object>,  f:Function )=>{
     
-console.log(f);
+for(let i in arr){
+  if(f(arr[i])){
+    console.log(arr[i]);
+  }
+}
+}
+filter(users,(e:any)=> {return e.age>20 });
+
+console.log('------------------------------------------------------')
+console.log('------------------------------------------------------')
+console.log("find:")
+var find =(arr:Array<object>, f:Function) =>{
+let foundVal =[];
+let j=0;
+  for(let i in arr){
+    if(f(arr[i])){
+      foundVal[j]=arr[i];
+      j++;
+    }
+  }
+  if(foundVal.length === 0){
+    console.log("not found");
+  }else{
+    console.log("filtered obj     =>   " );
+    for(let i in foundVal){
+      console.log(foundVal[i]);
+    }
+  }
 }
 
-filter(users,()=>{1+1});
+find(users,(e:any)=> {return e.age === 19 || e.active === true});
+
+console.log('------------------------------------------------------')
+console.log('------------------------------------------------------')
+console.log("reduce:")
+var reduce = (arr:Array<number>, f:Function , initVal :number) =>{
+let initSum = initVal ;
+let array = arr;
+
+for(let i=0;i<array.length;i++){
+  initSum = f(initSum,arr[i]);
+}
+return initSum;
+}
+
+let ans = reduce([1, 2], (sum : number, n :number)=> {
+  return sum + n;
+}, 0);
+
+console.log(ans);
